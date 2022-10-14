3e07fd514a06643c7dbb2eea0bd51ae963dc2c13
Anna <anyaamdmitrieva@yandex.ru>
2022-10-14 17:38:03 +0600

    add proj4

---
+++ 4/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int number2 = number1 * -1;
+
+if (number2 < number1)
+{
+    while (number2 <= number1)
+    {
+    Console.WriteLine(number2);
+    number2++;
+    }
+}
+
+else
+{
+    while (number1 <= number2)
+    {
+    Console.WriteLine(number1);
+    number1++;
+    }
+}
