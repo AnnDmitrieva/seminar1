3adb172ea08751056698b9cfdbb92ad9fdefc4ad
Anna <anyaamdmitrieva@yandex.ru>
2022-10-14 15:51:41 +0600

    add proj2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Ведите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ведите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int sqrNumber = number2 * number2;
+
+if (sqrNumber == number1)
+{
+   Console.Write("ДА!");
+}
+
+if (sqrNumber != number1)
+{
+    Console.Write("НЕТ!");
+}
