1e3a1120688fcb6c0bda8fa05ff94c4cf40fbad2
Anna <anyaamdmitrieva@yandex.ru>
2022-10-14 16:27:24 +0600

    add proj3

---
+++ 3/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ if (number > 7 | number < 1)
+ {
+    Console.Write("ОШИБКА! Введите число от 1 до 7");
+ }
+ if (number == 1)
+ {
+    Console.Write("Понедельник...(((");
+ }
+
+ if (number == 2)
+ {
+    Console.Write("Вторник..(");
+ }
+
+ if (number == 3)
+ {
+    Console.Write("Среда.");
+ }
+
+ if (number == 4)
+ {
+    Console.Write("Четверг)");
+ }
+
+ if (number == 5)
+ {
+    Console.Write("Пятница!!)))");
+ }
+
+ if (number == 6)
+ {
+    Console.Write("Суббота!%D");
+ }
+
+ if (number == 7)
+ {
+    Console.Write("Пока еще воскресенье)");
+ }
